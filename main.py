1eea803f17c6416cbbe9cb5490a8d1a784f9c329
Omar Quimbaya <omar.quimbaya4@gmail.com>
2021-09-10 14:11:13 -0500

    First commit.

---
+++ main.py
@@ -0,0 +1,23 @@
+import snyk
+from dotenv import dotenv_values
+
+
+def create_config() -> dict:
+    return dotenv_values(".env")
+
+
+def create_client(api_token: str = None) -> snyk.SnykClient:
+    if api_token is None:
+        raise ValueError
+    client = snyk.SnykClient(api_token)
+    return client
+
+
+def main() -> None:
+    config = create_config()
+    client = create_client(config["API_TOKEN"])
+    print(client.organizations.first().projects.all())
+
+
+if __name__ == '__main__':
+    main()
